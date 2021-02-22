508a81534770d7c8be6d754d717084314cdda6e1
Matthew Thomas <mzthomas96@gmail.com>
2021-02-21 20:54:44 -0500

    commit tree a229a2d6462cc93da080859fdf28bd618eb2352c

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+ let drivers = ["Milo", "Otis","Garfield"];
+function destructivelyAppendDriver (addition){
+    drivers.push(addition);
+ }
+ function destructivelyPrependDriver (addition){
+     drivers.unshift(addition);
+ }
+ function destructivelyRemoveLastDriver (addition){
+    drivers.pop(addition);
+ }
+ function destructivelyRemoveFirstDriver(addition){
+     drivers.shift(addition)
+ }
+ function appendDriver (name) {
+    return [...drivers, name];
+  }
+  
+  function prependDriver (name) {
+    return [name, ...drivers];
+  }
+  
+  function removeFirstDriver () {
+    return drivers.slice(1);
+  }
+  
+  function removeLastDriver () {
+    return drivers.slice(0, drivers.length - 1);
+  }
